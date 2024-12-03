36822f33a075cbab2593a1cdb83251c391bacc1a
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-12-03 17:51:52 +0700

    feat(api): use fastify instead of express

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
@@ -9,7 +13,10 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const configService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
@@ -30,7 +37,7 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, config),
   );
 
-  const port = configService.get<number>('PORT');
-  await app.listen(port);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
