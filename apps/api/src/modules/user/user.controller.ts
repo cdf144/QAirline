7279eefc044da62f18e70cda93bf32e34348c84b
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-12-03 20:57:21 +0700

    feat: add versioning to user API and return appropriate response

--- apps/api/src/modules/user/user.controller.ts
+++ apps/api/src/modules/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { FastifyReply } from 'fastify';
 import { RegisterDto } from './dto/register.dto';
-import { User } from './schemas/user.schema';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('v1/user')
 export class UserController {
   constructor(private UserService: UserService) {}
+
   @Post('register')
-  async createUser(@Body() RegisterDto: RegisterDto): Promise<User> {
-    return this.UserService.createUser(RegisterDto);
+  async createUser(
+    @Res() res: FastifyReply,
+    @Body() RegisterDto: RegisterDto,
+  ): Promise<void> {
+    const user = await this.UserService.createUser(RegisterDto);
+    res.code(201).send(user);
   }
 }
