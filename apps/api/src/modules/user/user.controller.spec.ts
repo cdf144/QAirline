c137d63ed62cf640c13694da388793bedeb83f0f
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-12-04 11:03:46 +0700

    test: make user jest tests work
    Apparently absolute paths imports are not working in the tests, so they
    need to be changed to relative paths.
    
    The added mock models are obvious.

--- apps/api/src/modules/user/user.controller.spec.ts
+++ apps/api/src/modules/user/user.controller.spec.ts
@@ -1,12 +1,27 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  const mockUserModel = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken('User'),
+          useValue: mockUserModel,
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
--- apps/api/src/modules/user/user.service.spec.ts
+++ apps/api/src/modules/user/user.service.spec.ts
@@ -1,12 +1,26 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from './schemas/user.schema';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
+  const mockUserModel = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockUserModel,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
