6598e2e3f339c8f9f3b092f38f26d385825336a7
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-12-19 23:36:47 +0700

    chore: rename component directory to 'components'

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { AirlineLogin } from "./component/AirlineLogin.tsx";
-import { BookingPage } from "./component/BookingPage.tsx";
-import BookingPageAfterClickInRecommend from "./component/BookingPageAfterClickInRecommend.tsx";
-import ForgotPassword from "./component/ForgotPassword.tsx";
-import { HomePage } from "./component/HomePage.tsx";
-import { ManageBooking } from "./component/ManageBooking.tsx";
-import { SignUpPage } from "./component/SignUpPage.tsx";
+import { AirlineLogin } from "./components/AirlineLogin.tsx";
+import { BookingPage } from "./components/BookingPage.tsx";
+import BookingPageAfterClickInRecommend from "./components/BookingPageAfterClickInRecommend.tsx";
+import ForgotPassword from "./components/ForgotPassword.tsx";
+import { HomePage } from "./components/HomePage.tsx";
+import { ManageBooking } from "./components/ManageBooking.tsx";
+import { SignUpPage } from "./components/SignUpPage.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
