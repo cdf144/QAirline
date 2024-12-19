3f8bfd5a7857e836e0d7c0b7482b550b46cf2e86
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-12-19 17:53:18 +0700

    test: delete other tests and update user tests to work

--- apps/api/src/modules/user/user.controller.spec.ts
+++ apps/api/src/modules/user/user.controller.spec.ts
@@ -1,33 +1,136 @@
-import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { FastifyReply } from 'fastify';
+import { Types } from 'mongoose';
+import { Role } from 'src/common/enums/role.enum';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Gender, User } from './schemas/user.schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
+  let mockUserService: Partial<UserService>;
 
-  const mockUserModel = {
-    findOne: jest.fn(),
-    create: jest.fn(),
-    save: jest.fn(),
+  const mockUser: User = {
+    _id: Types.ObjectId.createFromHexString('48f256b4d353fd5c75739500'),
+    __v: 0,
+    email: 'test@example.com',
+    password: 'password',
+    fullName: 'Test User',
+    sex: Gender.Male,
+    phone: '+84123456789',
+    idCardNumber: '000000000000',
+    createdAt: new Date(),
+    roles: [Role.User],
   };
 
   beforeEach(async () => {
+    mockUserService = {
+      findAll: jest.fn().mockResolvedValue([mockUser]),
+      findOneByEmail: jest.fn().mockResolvedValue(mockUser),
+      create: jest.fn().mockResolvedValue(mockUser),
+      updateByEmail: jest.fn().mockResolvedValue(mockUser),
+      updateById: jest.fn().mockResolvedValue(mockUser),
+      deleteByEmail: jest.fn().mockResolvedValue(mockUser),
+      getIdentifierType: jest.fn().mockImplementation((identifier: string) => {
+        return Types.ObjectId.isValid(identifier) ? 'mongoId' : 'email';
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [
-        UserService,
-        {
-          provide: getModelToken('User'),
-          useValue: mockUserModel,
-        },
-      ],
+      providers: [{ provide: UserService, useValue: mockUserService }],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const res = {
+        send: jest.fn(),
+      } as unknown as FastifyReply;
+
+      await controller.findAll(res);
+
+      expect(service.findAll).toHaveBeenCalled();
+      expect(res.send).toHaveBeenCalledWith([mockUser]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user', async () => {
+      const res = {
+        send: jest.fn(),
+      } as unknown as FastifyReply;
+
+      await controller.findOne(res, 'test@example.com');
+
+      expect(service.findOneByEmail).toHaveBeenCalledWith('test@example.com');
+      expect(res.send).toHaveBeenCalledWith(mockUser);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const res = {
+        send: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+      } as unknown as FastifyReply;
+
+      const registerUserDto: RegisterUserDto = {
+        email: 'test@example.com',
+        password: 'password',
+        fullName: 'Test User',
+        sex: Gender.Male,
+        phone: '+84123456789',
+        idCardNumber: '000000000000',
+      };
+
+      await controller.create(res, registerUserDto);
+
+      expect(service.create).toHaveBeenCalledWith(registerUserDto);
+      expect(res.send).toHaveBeenCalledWith(mockUser);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user', async () => {
+      const res = {
+        send: jest.fn(),
+      } as unknown as FastifyReply;
+
+      const updateUserDto: UpdateUserDto = {
+        fullName: 'Updated User',
+      };
+
+      await controller.update(res, '48f256b4d353fd5c75739500', updateUserDto);
+
+      expect(service.updateById).toHaveBeenCalledWith(
+        '48f256b4d353fd5c75739500',
+        updateUserDto,
+      );
+      expect(res.send).toHaveBeenCalledWith(mockUser);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a user', async () => {
+      const res = {
+        send: jest.fn(),
+      } as unknown as FastifyReply;
+
+      await controller.delete(res, 'test@example.com');
+
+      expect(service.deleteByEmail).toHaveBeenCalledWith('test@example.com');
+      expect(res.send).toHaveBeenCalledWith(mockUser);
+    });
   });
 });
--- apps/api/src/modules/user/user.service.spec.ts
+++ apps/api/src/modules/user/user.service.spec.ts
@@ -1,16 +1,35 @@
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { User } from './schemas/user.schema';
+import { Model, Types } from 'mongoose';
+import { Role } from 'src/common/enums/role.enum';
+import { Gender, User } from './schemas/user.schema';
 import { UserService } from './user.service';
+
+const mockUser = {
+  _id: Types.ObjectId.createFromHexString('48f256b4d353fd5c75739500'),
+  __v: 0,
+  email: 'test@example.com',
+  password: 'password',
+  fullName: 'John Doe',
+  sex: Gender.Male,
+  phone: '+84123456789',
+  idCardNumber: null,
+  createdAt: new Date('2024-01-01T00:30:00.000Z'),
+  roles: [Role.User],
+};
+
+const mockUserModel = {
+  find: jest.fn(),
+  findOne: jest.fn(),
+  create: jest.fn(),
+  findOneAndUpdate: jest.fn(),
+  findOneAndDelete: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UserService;
-
-  const mockUserModel = {
-    findOne: jest.fn(),
-    create: jest.fn(),
-    save: jest.fn(),
-  };
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let model: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,9 +43,28 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
+    model = module.get<Model<User>>(getModelToken(User.name));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return a user by id', async () => {
+    jest.spyOn(service, 'findOneById').mockResolvedValueOnce(mockUser);
+
+    const result = await service.findOneById('48f256b4d353fd5c75739500');
+    expect(result).toEqual(mockUser);
+    expect(service['findOneById']).toHaveBeenCalledWith(
+      '48f256b4d353fd5c75739500',
+    );
+  });
+
+  it('should return a user by email', async () => {
+    jest.spyOn(service, 'findOneByEmail').mockResolvedValueOnce(mockUser);
+
+    const result = await service.findOneByEmail('test@example.com');
+    expect(result).toEqual(mockUser);
+    expect(service['findOneByEmail']).toHaveBeenCalledWith('test@example.com');
   });
 });
